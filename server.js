c0d944b55be6b1fab7867aa5b8abb7581f28ed52
NOUGLOZEH <gerard.nouglozeh@atos.net>
2019-12-10 15:00:42 +0100

    Mise a jour du serveur

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.get('/*', (res, res) => {
     res.sendFile(path.join(__dirname, '/dist/myapp/index.html'));
 });
 
-app.listen(porcess.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Server started on port 3000!')
 })
