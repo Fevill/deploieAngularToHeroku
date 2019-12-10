5ad9ccc1e8127690747490c21d5a6578135be09f
NOUGLOZEH <gerard.nouglozeh@atos.net>
2019-12-10 14:50:02 +0100

    Configuration du  package json et mise en place du serveur node Js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+path = require('path');
+const app = express();
+
+app.use(express.static('./dist/myapp'))
+
+app.get('/*', (res, res) => {
+    res.sendFile(path.join(__dirname, '/dist/myapp/index.html'));
+});
+
+app.listen(porcess.env.PORT || 3000, () => {
+    console.log('Server started on port 3000!')
+})
